733e2a46efcd0e844cd92c766a208ec9afde2693
UCDT <adrian.j.ulibarri@gmail.com>
2021-07-30 08:59:19 -0600

    added Post.js

---
+++ models/Post.js
@@ -0,0 +1,43 @@
+const { Model, Datatype, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+//create the Post model
+
+class Post extends Model {}
+
+//create fields/columns for Post model
+
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        post_url: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                idURL: true
+            }
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post'
+    }
+);
