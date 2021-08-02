5d5fb591386cfa4afb6f1d9b112b08c30ff06c75
UCDT <adrian.j.ulibarri@gmail.com>
2021-08-02 09:01:08 -0600

    finished work

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 const User = require('./User');
 const Post = require('./Post');
 const Vote = require('./Vote');
+const Comment = require('./Comment');
 
 // create associations
 User.hasMany(Post, {
@@ -27,6 +28,13 @@ Vote.belongsTo(Post, {
     foreignKey: 'post_id'
 });
 
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
 
 // many to many associations
 
@@ -36,10 +44,19 @@ User.belongsToMany(Post, {
     foreignKey: 'user_id'
 });
 
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+
 Post.belongsToMany(User, {
     through: Vote,
     as: 'voted_posts',
     foreignKey: 'post_id'
 });
 
-module.exports = { User, Post, Vote };
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
+
+
+module.exports = { User, Post, Vote, Comment };
