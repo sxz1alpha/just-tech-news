d020e63f4ab38c1098b1b2994e35367609379889
UCDT <adrian.j.ulibarri@gmail.com>
2021-08-02 09:11:23 -0600

    connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -3,10 +3,16 @@ const Sequelize = require('sequelize');
 require('dotenv').config(); 
 
 // create connection to our database
-const sequelize = new Sequelize(process.env.DB_name, process.env.DB_user, process.env.DB_pass, {
-    host: 'localhost',
-    dialect: 'mysql',
-    post: 3306
-});
+let sequelize;
+
+if( process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+    sequelize = new Sequelize(process.env.DB_name, process.env.DB_user, process.env.DB_pass, {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306
+    });
+}
 
 module.exports = sequelize;
