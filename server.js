95e7f1a39d7ce93b6e755a6ed6cd172277dd931c
UCDT <adrian.j.ulibarri@gmail.com>
2021-08-02 20:03:27 -0600

    commiting from root

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const session = require('express-session');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
-const hbs = exphbs.create({});
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
